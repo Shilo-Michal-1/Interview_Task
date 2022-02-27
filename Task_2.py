11ed06f09bf7c562e6890a59e669c95829872e87
Michal <shilomichali@gmaul.com>
2022-02-27 09:41:06 +0200

    upload

--- Task_2.py
+++ Task_2.py
@@ -16,16 +16,19 @@ def validate_json(json_data):
 
 if __name__ == '__main__':
 
-    if path.exists('valid_json.json'):
-        with open(r'valid_json.json', 'r', encoding='utf-8') as f:
+    valid_json_path = 'valid_json.json'
+    not_valid_json_path = 'not_valid_json.json'
+
+    if path.exists(valid_json_path):
+        with open(valid_json_path, 'r', encoding='utf-8') as f:
             json_file = f.read()
         validate_json(json_file)
     else:
         print("File path is incorrect, Please make sure the file exists")
         # output : Given JSON string is Valid True
 
-    if path.exists('not_valid_json.json'):
-        with open(r'not_valid_json.json', 'r', encoding='utf-8') as f:
+    if path.exists(not_valid_json_path):
+        with open(not_valid_json_path, 'r', encoding='utf-8') as f:
             json_file = f.read()
         validate_json(json_file)
     else:
