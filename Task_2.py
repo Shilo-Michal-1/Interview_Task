be614ae50e6c598faeb691b0ee1ecc6b218fa547
Michal <shilomichali@gmaul.com>
2022-02-27 02:58:51 +0200

    upload

--- Task_2.py
+++ Task_2.py
@@ -1,4 +1,5 @@
 import json
+from os import path
 
 # Function validate a JSON file
 
@@ -6,18 +7,27 @@ import json
 def validate_json(json_data):
     try:
         json.loads(json_data)
+        print("Given JSON string is Valid true")
     except ValueError as err:
+        print("Given JSON string is Valid false,", err)
         return False
     return True
 
 
-with open(r'valid_json.json', 'r', encoding='utf-8') as f:
-    json_file = f.read()
-print("Given JSON string is Valid", validate_json(json_file))
-# output : Given JSON string is Valid True
+if __name__ == '__main__':
 
+    if path.exists('valid_json.json'):
+        with open(r'valid_json.json', 'r', encoding='utf-8') as f:
+            json_file = f.read()
+        validate_json(json_file)
+    else:
+        print("File path is incorrect, Please make sure the file exists")
+        # output : Given JSON string is Valid True
 
-with open(r'not_valid_json.json', 'r', encoding='utf-8') as f:
-    json_file = f.read()
-print("Given JSON string is Valid", validate_json(json_file))
-# output : Given JSON string is Valid False
+    if path.exists('not_valid_json.json'):
+        with open(r'not_valid_json.json', 'r', encoding='utf-8') as f:
+            json_file = f.read()
+        validate_json(json_file)
+    else:
+        print("File path is incorrect, Please make sure the file exists")
+        # output : Given JSON string is Valid False, error
